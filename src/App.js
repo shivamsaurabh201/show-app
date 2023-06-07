bdb3dc47fc693d1974b747704ff4ff26371d8b8b
Shivam Saurabh <shivamsaurabh201@gmail.com>
2023-06-07 12:58:12 +0530

    index app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import ShowList from './ShowList';
-import ShowDetails from './ShowDetails';
+// import React, { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// import ShowList from './ShowList';
+// import ShowDetails from './ShowDetails';
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <Router>
+//       <div>
+//         <nav>
+//           <ul>
+//             <li>
+//               <Link to="/">Home</Link>
+//             </li>
+//           </ul>
+//         </nav>
+
+//         <Switch>
+//           <Route exact path="/" component={ShowList} />
+//           <Route path="/show/:id" component={ShowDetails} />
+//         </Switch>
+//       </div>
+//     </Router>
+//   );
+// };
+
+// export default App;
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import ShowSummary from './components/ShowSummary';
+
+function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Switch>
-          <Route exact path="/" component={ShowList} />
-          <Route path="/show/:id" component={ShowDetails} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/summary/:id" component={ShowSummary} />
+      </Switch>
     </Router>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+// import { BrowserRouter } from 'react-router-dom';
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+//   document.getElementById('root')
+// );
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <BrowserRouter>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
+  </React.StrictMode>,
   document.getElementById('root')
 );
